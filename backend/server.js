2c48350091e4e4b6c05b410ea4985e3b16920e35
its-me-Harsh-Anand <harshvivek.787@gmail.com>
2021-11-25 22:40:04 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 5000
 const app = express()
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors()) //allows us to fetch API cross platform (without CORS error)
+app.use(express.json()) //this will allow us to parse the json
 
 //mongoDB connection
 const uri = process.env.ATLAS_URI
@@ -18,6 +18,13 @@ const connection = mongoose.connection
 connection.once('open', ()=>{
     console.log(`MongoDB database connected successfully - uri = ${uri}`)
 })
+
+//use the file inside routes
+const excercisesRouter = require('./routes/excercises')
+const usersRouter = require('./routes/users')
+
+app.use('/excercises', excercisesRouter)
+app.use('/users', usersRouter)
 
 //listening server
 app.listen(port, ()=>{
